847188b5f356ad8ce48c1fb4311a55a53503c4a5
Tom Eaves <Tom.Eaves@compare.com>
2017-11-20 10:51:26 -0500

    Initial commit

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-page',
+  templateUrl: './landing-page.template.html',
+  styleUrls: ['./landing-page.styles.less']
+})
+export class LandingPageComponent {
+  
+}
---
+++ src/app/participant-sign-up/participant-sign-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'participant-sign-up',
+  templateUrl: './participant-sign-up.template.html',
+  styleUrls: ['./participant-sign-up.styles.less']
+})
+export class ParticipantSignUpComponent {
+  
+}
